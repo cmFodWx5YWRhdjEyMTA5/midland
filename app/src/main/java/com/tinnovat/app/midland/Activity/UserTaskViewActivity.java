6ecbd9b527de4d3bb9a2526a0953fca3f00413ee
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-05-23 18:04:08 +0530

    User task view

--- app/src/main/java/com/tinnovat/app/midland/Activity/UserTaskViewActivity.java
+++ app/src/main/java/com/tinnovat/app/midland/Activity/UserTaskViewActivity.java
@@ -2,12 +2,48 @@ package com.tinnovat.app.midland.Activity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.tinnovat.app.midland.model.Data;
+import com.tinnovat.app.midland.network.ApiClient;
+import com.tinnovat.app.midland.network.ApiInterface;
+import com.tinnovat.app.midland.network.model.request.ADLoginRequest;
+import com.tinnovat.app.midland.network.model.request.DataRequestBody;
+import com.tinnovat.app.midland.network.model.request.DataRowRequest;
+import com.tinnovat.app.midland.network.model.request.FieldData;
+import com.tinnovat.app.midland.network.model.request.ModelCRUD;
+import com.tinnovat.app.midland.network.model.request.ModelCRUDRequest;
+import com.tinnovat.app.midland.network.model.request.RequestData;
+import com.tinnovat.app.midland.network.model.request.RequestEnvelope;
+import com.tinnovat.app.midland.network.model.response.DataRow;
+import com.tinnovat.app.midland.network.model.response.FieldDataResponse;
+import com.tinnovat.app.midland.network.model.response.ResponseEnvelope;
+import com.tinnovat.app.midland.network.model.response.WindowTabData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UserTaskViewActivity extends AppCompatActivity {
+
+    TextView assgnedBy;
+    TextView assgnedDate;
+    TextView estdStartDate;
+    TextView estdEndDate;
+    TextView reStartDate;
+    TextView reEndDate;
+    TextView description;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -16,5 +52,138 @@ public class UserTaskViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_task_view);
         Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("User Task View");
+
+        assgnedBy = findViewById(R.id.assignedBy);
+        assgnedDate = findViewById(R.id.assignedDate);
+        estdStartDate = findViewById(R.id.estdStartDate);
+        estdEndDate = findViewById(R.id.estdEndDate);
+        reStartDate = findViewById(R.id.reStartDate);
+        reEndDate = findViewById(R.id.reEndDate);
+        description = findViewById(R.id.description);
+
+        initiateService();
+    }
+    public void setData(Data responseData){
+        Toast.makeText(UserTaskViewActivity.this,"Success",Toast.LENGTH_SHORT).show();
+        assgnedBy.setText(responseData.getDataSet().get(0).get("assignedby"));
+        assgnedDate.setText(responseData.getDataSet().get(0).get("assigneddate"));
+        estdStartDate.setText(responseData.getDataSet().get(0).get("EstimatedStartDate"));
+        estdEndDate.setText(responseData.getDataSet().get(0).get("estimatedenddate"));
+        reStartDate.setText(responseData.getDataSet().get(0).get("resheduledstartdate"));
+        reEndDate.setText(responseData.getDataSet().get(0).get("resheduledenddate"));
+        description.setText(responseData.getDataSet().get(0).get("Description"));
+
+
+
+    }
+    private void initiateService() {
+
+        //TODO add or change methods accordingly
+        RequestEnvelope envelope = getRequestEnvelopeGeneral();
+
+        Call<ResponseEnvelope> call = ApiClient.getApiClient().create(ApiInterface.class).fetchData(envelope);
+        call.enqueue(new Callback<ResponseEnvelope>() {
+            @Override
+            public void onResponse(Call<ResponseEnvelope> call, Response<ResponseEnvelope> response) {
+                Log.e("Success","Success");
+
+                Data responseData = getParsedData(response.body().getBody().getQueryDataResponse().getData());
+                setData(responseData);
+                Toast.makeText(UserTaskViewActivity.this,"Success",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseEnvelope> call, Throwable t) {
+                Log.e("Success","Success");
+                Toast.makeText(UserTaskViewActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+
+    @NonNull
+    private RequestEnvelope getRequestEnvelopeGeneral() {
+        RequestEnvelope envelope = new RequestEnvelope();
+
+        DataRequestBody body = new DataRequestBody();
+
+        final RequestData data = new RequestData();
+
+        ModelCRUDRequest modelCRUDRequest = new ModelCRUDRequest();
+
+        ADLoginRequest loginRequest = new ADLoginRequest();
+
+        ModelCRUD modelCRUD = new ModelCRUD();
+
+        DataRowRequest dataRow = new DataRowRequest();
+
+
+        //TODO set all available data's here
+        loginRequest.setClientID(1000000);
+        loginRequest.setUser("SuperUser");
+        loginRequest.setPass("System");
+        loginRequest.setLang("en_US");
+        loginRequest.setRoleID(1000000);
+        loginRequest.setOrgID(1000000);
+        loginRequest.setWarehouseID(1000004);
+        loginRequest.setStage(0);
+
+
+        // Params inside dataRow as list (can use a loop or simply add objects to list
+        List<FieldData> fieldDataList = new ArrayList<>();
+
+        FieldData fieldData = new FieldData("SC_UserTask_ID","1000013");
+
+        fieldDataList.add(fieldData);
+
+        dataRow.setField(fieldDataList);
+
+        // Set modelCurd
+        modelCRUD.setDataRow(dataRow);
+        modelCRUD.setServiceType("MLW_UserTaskView_Web");
+        modelCRUD.setTableName("MLV_UserTaskView_View");
+        //  modelCRUD.setAction("action");
+
+        // Set modelCurdRequest
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        modelCRUDRequest.setModelCRUD(modelCRUD);
+
+        // Set modelCurdRequest to RequestData
+        data.setCduRequest(modelCRUDRequest);
+
+        //Set RequestData object to DataRequestBody
+        body.setUsStatesRequestData(data);
+
+        //Set DataRequestBody object to RequestEnvelope
+        envelope.setBody(body);
+
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        return envelope;
+    }
+
+    private Data getParsedData(WindowTabData tabData) {
+
+        if (tabData.getDataSet() != null && tabData.getDataSet().getDataRowList() != null && !tabData.getDataSet().getDataRowList().isEmpty()) {
+            List rowList = new ArrayList();
+            for (DataRow item : tabData.getDataSet().getDataRowList()) {
+                Map<String, String> fetchedContent = new HashMap<>();
+                for (FieldDataResponse mapItem : item.getFieldData()) {
+                    fetchedContent.put(mapItem.getColumn(), mapItem.getVal());
+                }
+                rowList.add(fetchedContent);
+            }
+
+            Data data = new Data();
+            //  data.setNumRows(tabData.getNumRows());
+            data.setRowCount(tabData.getRowCount());
+            // data.setStartRow(tabData.getStartRow());
+            //  data.setTotalRows(tabData.getTotalRows());
+            data.setSuccess(tabData.isSuccess());
+            data.setDataSet(rowList);
+            return data;
+        } else
+            return null;
     }
 }
