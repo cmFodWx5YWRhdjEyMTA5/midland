7a3ea650e03e98cd764bc4a76a29515e4b0352c4
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-05-23 14:49:49 +0530

    Product Requisition Approval view

--- app/src/main/java/com/tinnovat/app/midland/Activity/ProductRequisitionApprovalActivity.java
+++ app/src/main/java/com/tinnovat/app/midland/Activity/ProductRequisitionApprovalActivity.java
@@ -2,12 +2,49 @@ package com.tinnovat.app.midland.Activity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.tinnovat.app.midland.model.Data;
+import com.tinnovat.app.midland.network.ApiClient;
+import com.tinnovat.app.midland.network.ApiInterface;
+import com.tinnovat.app.midland.network.model.request.ADLoginRequest;
+import com.tinnovat.app.midland.network.model.request.DataRequestBody;
+import com.tinnovat.app.midland.network.model.request.DataRowRequest;
+import com.tinnovat.app.midland.network.model.request.FieldData;
+import com.tinnovat.app.midland.network.model.request.ModelCRUD;
+import com.tinnovat.app.midland.network.model.request.ModelCRUDRequest;
+import com.tinnovat.app.midland.network.model.request.RequestData;
+import com.tinnovat.app.midland.network.model.request.RequestEnvelope;
+import com.tinnovat.app.midland.network.model.response.DataRow;
+import com.tinnovat.app.midland.network.model.response.FieldDataResponse;
+import com.tinnovat.app.midland.network.model.response.ResponseEnvelope;
+import com.tinnovat.app.midland.network.model.response.WindowTabData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ProductRequisitionApprovalActivity extends AppCompatActivity {
+
+    TextView org;
+    TextView documentNo;
+    TextView userName;
+    TextView Descrptn;
+    TextView comnt;
+    TextView warehuse;
+    TextView priceList;
+
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -16,5 +53,138 @@ public class ProductRequisitionApprovalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_requisition_approval);
         Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("Product Requisition Approval");
+
+        org = findViewById(R.id.org);
+        documentNo = findViewById(R.id.docNo);
+        userName = findViewById(R.id.user);
+        Descrptn = findViewById(R.id.description);
+        comnt = findViewById(R.id.comment);
+        warehuse = findViewById(R.id.warehouse);
+        priceList = findViewById(R.id.priceList);
+
+        initiateService();
+    }
+
+    public void setData(Data responseData){
+        Toast.makeText(ProductRequisitionApprovalActivity.this,"Success",Toast.LENGTH_SHORT).show();
+        org.setText(responseData.getDataSet().get(0).get("OrgName"));
+        documentNo.setText(responseData.getDataSet().get(0).get("DocumentNo"));
+        userName.setText(responseData.getDataSet().get(0).get("UserName"));
+        Descrptn.setText(responseData.getDataSet().get(0).get("Description"));
+        comnt.setText(responseData.getDataSet().get(0).get("Help"));
+        warehuse.setText(responseData.getDataSet().get(0).get("warehouse"));
+        priceList.setText(responseData.getDataSet().get(0).get("PriceList"));
+
+
+    }
+    private void initiateService() {
+
+        //TODO add or change methods accordingly
+        RequestEnvelope envelope = getRequestEnvelopeGeneral();
+
+        Call<ResponseEnvelope> call = ApiClient.getApiClient().create(ApiInterface.class).fetchData(envelope);
+        call.enqueue(new Callback<ResponseEnvelope>() {
+            @Override
+            public void onResponse(Call<ResponseEnvelope> call, Response<ResponseEnvelope> response) {
+                Log.e("Success","Success");
+
+                Data responseData = getParsedData(response.body().getBody().getQueryDataResponse().getData());
+                setData(responseData);
+                Toast.makeText(ProductRequisitionApprovalActivity.this,"Success",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseEnvelope> call, Throwable t) {
+                Log.e("Success","Success");
+                Toast.makeText(ProductRequisitionApprovalActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+
+    @NonNull
+    private RequestEnvelope getRequestEnvelopeGeneral() {
+        RequestEnvelope envelope = new RequestEnvelope();
+
+        DataRequestBody body = new DataRequestBody();
+
+        final RequestData data = new RequestData();
+
+        ModelCRUDRequest modelCRUDRequest = new ModelCRUDRequest();
+
+        ADLoginRequest loginRequest = new ADLoginRequest();
+
+        ModelCRUD modelCRUD = new ModelCRUD();
+
+        DataRowRequest dataRow = new DataRowRequest();
+
+
+        //TODO set all available data's here
+        loginRequest.setClientID(1000000);
+        loginRequest.setUser("SuperUser");
+        loginRequest.setPass("System");
+        loginRequest.setLang("en_US");
+        loginRequest.setRoleID(1000000);
+        loginRequest.setOrgID(1000000);
+        loginRequest.setWarehouseID(1000004);
+        loginRequest.setStage(0);
+
+
+        // Params inside dataRow as list (can use a loop or simply add objects to list
+        List<FieldData> fieldDataList = new ArrayList<>();
+
+        FieldData fieldData = new FieldData("M_Requisition_ID","1000031");
+
+        fieldDataList.add(fieldData);
+
+        dataRow.setField(fieldDataList);
+
+        // Set modelCurd
+        modelCRUD.setDataRow(dataRow);
+        modelCRUD.setServiceType("MLW_ProductRequisition_Web");
+        modelCRUD.setTableName("MLV_ProductRequisition_View");
+        //  modelCRUD.setAction("action");
+
+        // Set modelCurdRequest
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        modelCRUDRequest.setModelCRUD(modelCRUD);
+
+        // Set modelCurdRequest to RequestData
+        data.setCduRequest(modelCRUDRequest);
+
+        //Set RequestData object to DataRequestBody
+        body.setUsStatesRequestData(data);
+
+        //Set DataRequestBody object to RequestEnvelope
+        envelope.setBody(body);
+
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        return envelope;
+    }
+
+    private Data getParsedData(WindowTabData tabData) {
+
+        if (tabData.getDataSet() != null && tabData.getDataSet().getDataRowList() != null && !tabData.getDataSet().getDataRowList().isEmpty()) {
+            List rowList = new ArrayList();
+            for (DataRow item : tabData.getDataSet().getDataRowList()) {
+                Map<String, String> fetchedContent = new HashMap<>();
+                for (FieldDataResponse mapItem : item.getFieldData()) {
+                    fetchedContent.put(mapItem.getColumn(), mapItem.getVal());
+                }
+                rowList.add(fetchedContent);
+            }
+
+            Data data = new Data();
+            //  data.setNumRows(tabData.getNumRows());
+            data.setRowCount(tabData.getRowCount());
+            // data.setStartRow(tabData.getStartRow());
+            //  data.setTotalRows(tabData.getTotalRows());
+            data.setSuccess(tabData.isSuccess());
+            data.setDataSet(rowList);
+            return data;
+        } else
+            return null;
     }
 }
