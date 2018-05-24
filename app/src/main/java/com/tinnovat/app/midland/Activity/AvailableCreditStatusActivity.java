44ed3f04a2ef27d63f14532825967cdcd0229192
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-05-24 15:09:09 +0530

    Available Credit Status

--- app/src/main/java/com/tinnovat/app/midland/Activity/AvailableCreditStatusActivity.java
+++ app/src/main/java/com/tinnovat/app/midland/Activity/AvailableCreditStatusActivity.java
@@ -2,12 +2,48 @@ package com.tinnovat.app.midland.Activity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.tinnovat.app.midland.model.Data;
+import com.tinnovat.app.midland.network.ApiClient;
+import com.tinnovat.app.midland.network.ApiInterface;
+import com.tinnovat.app.midland.network.model.request.ADLoginRequest;
+import com.tinnovat.app.midland.network.model.request.DataRowRequest;
+import com.tinnovat.app.midland.network.model.request.FieldData;
+import com.tinnovat.app.midland.network.model.request.ModelCRUD;
+import com.tinnovat.app.midland.network.model.request.ModelCRUDRequest;
+import com.tinnovat.app.midland.network.model.request.query.QueryDataRequestBody;
+import com.tinnovat.app.midland.network.model.request.query.QueryRequestData;
+import com.tinnovat.app.midland.network.model.request.query.QueryRequestEnvelope;
+import com.tinnovat.app.midland.network.model.response.query.DataRow;
+import com.tinnovat.app.midland.network.model.response.query.FieldDataResponse;
+import com.tinnovat.app.midland.network.model.response.query.ResponseQueryEnvelope;
+import com.tinnovat.app.midland.network.model.response.query.WindowTabData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AvailableCreditStatusActivity extends AppCompatActivity {
+
+    TextView partner;
+    TextView pendingCredit;
+    TextView pendingDebit;
+    TextView onHandBalance;
+    TextView availableCredit;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -16,5 +52,131 @@ public class AvailableCreditStatusActivity extends AppCompatActivity {
         setContentView(R.layout.activity_available_credit_status);
         Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("Available Credit Status");
+
+        partner = findViewById(R.id.partner);
+        pendingCredit = findViewById(R.id.pendingCredit);
+        pendingDebit = findViewById(R.id.pendingDebit);
+        onHandBalance = findViewById(R.id.onHandBalance);
+        availableCredit = findViewById(R.id.availableCredit);
+
+        initiateService();
+    }
+
+    public void setData(Data responseData){
+        Toast.makeText(AvailableCreditStatusActivity.this,"Success",Toast.LENGTH_SHORT).show();
+        partner.setText((responseData.getDataSet().get(0).get("businesspartner")));
+        pendingCredit.setText((responseData.getDataSet().get(0).get("PendingCreditApproval")));
+        pendingDebit.setText((responseData.getDataSet().get(0).get("PendingRequestApproval")));
+        onHandBalance.setText((responseData.getDataSet().get(0).get("OnHandBalance")));
+        availableCredit.setText((responseData.getDataSet().get(0).get("AvailableCredit")));
+    }
+
+    private void initiateService() {
+
+        //TODO add or change methods accordingly
+        QueryRequestEnvelope envelope = getRequestEnvelopeGeneral();
+
+        Call<ResponseQueryEnvelope> call = ApiClient.getApiClient().create(ApiInterface.class).fetchQueryData(envelope);
+        call.enqueue(new Callback<ResponseQueryEnvelope>() {
+            @Override
+            public void onResponse(Call<ResponseQueryEnvelope> call, Response<ResponseQueryEnvelope> response) {
+                Log.e("Success","Success");
+
+                Data responseData = getParsedData(response.body().getBody().getQueryDataResponse().getData());
+                setData(responseData);
+                Toast.makeText(AvailableCreditStatusActivity.this,"Success",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseQueryEnvelope> call, Throwable t) {
+                Log.e("Success","Success");
+                Toast.makeText(AvailableCreditStatusActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @NonNull
+    private QueryRequestEnvelope getRequestEnvelopeGeneral() {
+        QueryRequestEnvelope envelope = new QueryRequestEnvelope();
+
+        QueryDataRequestBody body = new QueryDataRequestBody();
+
+        final QueryRequestData data = new QueryRequestData();
+
+        ModelCRUDRequest modelCRUDRequest = new ModelCRUDRequest();
+
+        ADLoginRequest loginRequest = new ADLoginRequest();
+
+        ModelCRUD modelCRUD = new ModelCRUD();
+
+        DataRowRequest dataRow = new DataRowRequest();
+
+
+        //TODO set all available data's here
+        loginRequest.setClientID(1000000);
+        loginRequest.setUser("SuperUser");
+        loginRequest.setPass("System");
+        loginRequest.setLang("en_US");
+        loginRequest.setRoleID(1000000);
+        loginRequest.setOrgID(1000000);
+        loginRequest.setWarehouseID(1000004);
+        loginRequest.setStage(0);
+
+
+        // Params inside dataRow as list (can use a loop or simply add objects to list
+        List<FieldData> fieldDataList = new ArrayList<>();
+
+        FieldData fieldData = new FieldData("UserDailyActivity_ID","1000053");
+
+        fieldDataList.add(fieldData);
+
+        dataRow.setField(fieldDataList);
+
+        // Set modelCurd
+        modelCRUD.setDataRow(dataRow);
+        modelCRUD.setServiceType("MLW_cashUtilization_AvailCrView_View");
+        modelCRUD.setTableName("mlv_cashUtilization_AvailCr_view");
+        //  modelCRUD.setAction("action");
+
+        // Set modelCurdRequest
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        modelCRUDRequest.setModelCRUD(modelCRUD);
+
+        // Set modelCurdRequest to QueryRequestData
+        data.setCduRequest(modelCRUDRequest);
+
+        //Set QueryRequestData object to QueryDataRequestBody
+        body.setUsStatesRequestData(data);
+
+        //Set QueryDataRequestBody object to RequestEnvelope
+        envelope.setBody(body);
+
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        return envelope;
+    }
+
+    private Data getParsedData(WindowTabData tabData) {
+
+        if (tabData.getDataSet() != null && tabData.getDataSet().getDataRowList() != null && !tabData.getDataSet().getDataRowList().isEmpty()) {
+            List rowList = new ArrayList();
+            for (DataRow item : tabData.getDataSet().getDataRowList()) {
+                Map<String, String> fetchedContent = new HashMap<>();
+                for (FieldDataResponse mapItem : item.getFieldData()) {
+                    fetchedContent.put(mapItem.getColumn(), mapItem.getVal());
+                }
+                rowList.add(fetchedContent);
+            }
+
+            Data data = new Data();
+            //  data.setNumRows(tabData.getNumRows());
+            data.setRowCount(tabData.getRowCount());
+            // data.setStartRow(tabData.getStartRow());
+            //  data.setTotalRows(tabData.getTotalRows());
+            data.setSuccess(tabData.isSuccess());
+            data.setDataSet(rowList);
+            return data;
+        } else
+            return null;
     }
 }
