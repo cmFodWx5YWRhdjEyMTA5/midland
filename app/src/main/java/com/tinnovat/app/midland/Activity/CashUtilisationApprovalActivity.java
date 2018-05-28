17af66937df1be4dd2c827e0fa8255392f7e873a
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-05-28 12:27:39 +0530

    CashUtilisation Approval or Reject

--- app/src/main/java/com/tinnovat/app/midland/Activity/CashUtilisationApprovalActivity.java
+++ app/src/main/java/com/tinnovat/app/midland/Activity/CashUtilisationApprovalActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,10 +22,16 @@ import com.tinnovat.app.midland.network.model.request.ModelCRUD;
 import com.tinnovat.app.midland.network.model.request.ModelCRUDRequest;
 import com.tinnovat.app.midland.network.model.request.query.QueryRequestData;
 import com.tinnovat.app.midland.network.model.request.query.QueryRequestEnvelope;
+import com.tinnovat.app.midland.network.model.request.update.UpdateDataRequestBody;
+import com.tinnovat.app.midland.network.model.request.update.UpdateRequestData;
+import com.tinnovat.app.midland.network.model.request.update.UpdateRequestEnvelope;
+import com.tinnovat.app.midland.network.model.response.OutputField;
+import com.tinnovat.app.midland.network.model.response.StandardResponse;
 import com.tinnovat.app.midland.network.model.response.query.DataRow;
 import com.tinnovat.app.midland.network.model.response.query.FieldDataResponse;
 import com.tinnovat.app.midland.network.model.response.query.ResponseQueryEnvelope;
 import com.tinnovat.app.midland.network.model.response.query.WindowTabData;
+import com.tinnovat.app.midland.network.model.response.update.UpdateResponseEnvelope;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -48,6 +55,11 @@ public class CashUtilisationApprovalActivity extends AppCompatActivity {
     TextView frwdUser;
     TextView frwdRole;
     CheckBox frwrd;
+    TextView approve;
+    TextView reject;
+    private String mIsApproved = null;
+    private String mIsRejected = null;
+    private String mRequestId = null;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -67,9 +79,65 @@ public class CashUtilisationApprovalActivity extends AppCompatActivity {
         frwdUser = findViewById(R.id.frwdToUser);
         frwdRole = findViewById(R.id.frwdToRole);
         frwrd = findViewById(R.id.checkBoxForward);
+        approve = findViewById(R.id.approve);
+        reject = findViewById(R.id.reject);
 
         initiateService();
+
+        approve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                action("Y" ,"N");
+            }
+        });
+
+        reject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                action("N","Y");
+            }
+        });
     }
+
+    private void action( String approveVal , String rejectVal){
+        //TODO add or change methods accordingly
+        UpdateRequestEnvelope envelope = getUpdateRequestEnvelopeGeneral("IsApproved",approveVal,"SC_Rejected" ,rejectVal);
+
+        Call<UpdateResponseEnvelope> call = ApiClient.getApiClient().create(ApiInterface.class).fetchUpdateData(envelope);
+        call.enqueue(new Callback<UpdateResponseEnvelope>() {
+
+            @Override
+            public void onResponse(Call<UpdateResponseEnvelope> call, Response<UpdateResponseEnvelope> response) {
+
+                StandardResponse data = response.body().getBody().getQueryDataResponse().getData();
+                if (response.body().getBody().getQueryDataResponse().getData().getErrorMessage() == null && data.getFieldData() != null) {
+                    List<OutputField> list = data.getFieldData().getOutputFieldList();
+                    if (list != null && !list.isEmpty()) {
+                        for (OutputField listItem : list) {
+                            if (listItem.getColumn().equalsIgnoreCase("IsApproved"))
+                                mIsApproved = listItem.getVal();
+                            if (listItem.getColumn().equalsIgnoreCase("SC_Rejected"))
+                                mIsRejected = listItem.getVal();
+                            if (listItem.getColumn().equalsIgnoreCase("SC_Request_ID"))
+                                mRequestId = listItem.getVal();
+
+                        }
+                    }
+                }
+                if (mIsApproved != null && mIsRejected != null && mRequestId != null){
+                  //  finish();
+                    Toast.makeText(CashUtilisationApprovalActivity.this,"Completed "+mIsApproved+" : " +mIsRejected,Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UpdateResponseEnvelope> call, Throwable t) {
+                Toast.makeText(CashUtilisationApprovalActivity.this,"Failed ",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
     public void setData(Data responseData){
         Toast.makeText(CashUtilisationApprovalActivity.this,"Success",Toast.LENGTH_SHORT).show();
         notifiedBy.setText(responseData.getDataSet().get(0).get("notifiedby"));
@@ -98,7 +166,6 @@ public class CashUtilisationApprovalActivity extends AppCompatActivity {
         call.enqueue(new Callback<ResponseQueryEnvelope>() {
             @Override
             public void onResponse(Call<ResponseQueryEnvelope> call, Response<ResponseQueryEnvelope> response) {
-                Log.e("Success","Success");
 
                 Data responseData = getParsedData(response.body().getBody().getQueryDataResponse().getData());
                 setData(responseData);
@@ -107,13 +174,73 @@ public class CashUtilisationApprovalActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<ResponseQueryEnvelope> call, Throwable t) {
-                Log.e("Success","Success");
                 Toast.makeText(CashUtilisationApprovalActivity.this,"Failed",Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
+    @NonNull
+    private UpdateRequestEnvelope getUpdateRequestEnvelopeGeneral(String approve , String approveVal,String reject , String rejectVal) {
+
+        UpdateRequestEnvelope envelope = new UpdateRequestEnvelope();
+        UpdateDataRequestBody body = new UpdateDataRequestBody();
+        final UpdateRequestData data = new UpdateRequestData();
+        ModelCRUDRequest modelCRUDRequest = new ModelCRUDRequest();
+
+        ADLoginRequest loginRequest = new ADLoginRequest();
+
+        ModelCRUD modelCRUD = new ModelCRUD();
+
+        DataRowRequest dataRow = new DataRowRequest();
+
+
+        //TODO set all available data's here
+        loginRequest.setClientID(1000000);
+        loginRequest.setUser("SuperUser");
+        loginRequest.setPass("System");
+        loginRequest.setLang("en_US");
+        loginRequest.setRoleID(1000000);
+        loginRequest.setOrgID(1000000);
+        loginRequest.setWarehouseID(1000004);
+        loginRequest.setStage(0);
+
+        // Params inside dataRow as list (can use a loop or simply add objects to list
+        List<FieldData> fieldDataList = new ArrayList<>();
+
+      /* FieldData fieldDataApprove = new FieldData(approve,approveVal);
+       FieldData fieldDataReject = new FieldData(reject,rejectVal);*/
+
+        fieldDataList.add(new FieldData(approve,approveVal));
+        fieldDataList.add(new FieldData(reject,rejectVal));
+
+        // fieldDataList.add(fieldDataReject);
+
+        dataRow.setField(fieldDataList);
+
+        // Set modelCurd
+        modelCRUD.setDataRow(dataRow);
+        modelCRUD.setServiceType("MLW_CashRequisitionAprroval_Update");
+        modelCRUD.setRecordID("1000236");
+        // modelCRUD.setTableName("MLV_cashreq");
+        //  modelCRUD.setAction("action");
+
+        // Set modelCurdRequest
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        modelCRUDRequest.setModelCRUD(modelCRUD);
+
+        // Set modelCurdRequest to QueryRequestData
+        data.setCduRequest(modelCRUDRequest);
+
+        //Set QueryRequestData object to QueryDataRequestBody
+        body.setUsStatesRequestData(data);
+
+        //Set QueryDataRequestBody object to RequestEnvelope
+        envelope.setBody(body);
+
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        return envelope;
+    }
 
 
     @NonNull
