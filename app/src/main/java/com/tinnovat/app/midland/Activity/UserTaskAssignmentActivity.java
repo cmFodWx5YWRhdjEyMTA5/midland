a102ab3e39aae9d9c31eb541d7331ca3ae7b772f
rahultinnovat <38756318+rahultinnovat@users.noreply.github.com>
2018-05-29 12:52:47 +0530

    added spinner

--- app/src/main/java/com/tinnovat/app/midland/Activity/UserTaskAssignmentActivity.java
+++ app/src/main/java/com/tinnovat/app/midland/Activity/UserTaskAssignmentActivity.java
@@ -5,12 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.tinnovat.app.midland.model.Data;
 import com.tinnovat.app.midland.network.ApiClient;
 import com.tinnovat.app.midland.network.ApiInterface;
 import com.tinnovat.app.midland.network.model.request.ADLoginRequest;
@@ -25,6 +29,7 @@ import com.tinnovat.app.midland.network.model.request.query.QueryDataRequestBody
 import com.tinnovat.app.midland.network.model.request.query.QueryRequestData;
 import com.tinnovat.app.midland.network.model.request.query.QueryRequestEnvelope;
 import com.tinnovat.app.midland.network.model.response.OutputField;
+import com.tinnovat.app.midland.network.model.response.OutputFields;
 import com.tinnovat.app.midland.network.model.response.StandardResponse;
 import com.tinnovat.app.midland.network.model.response.create.CreateResponseEnvelope;
 import com.tinnovat.app.midland.network.model.response.query.ResponseQueryEnvelope;
@@ -50,6 +55,8 @@ public class UserTaskAssignmentActivity extends AppCompatActivity {
     TextView cancel;
     TextView createTask;
     String recordID = null;
+    Spinner spinnerAssignedTo;
+    String[] country = { "Supervisor1", "Purchase User", "Inventory User", "Supervisor", "Admin User9","MidlandUser","Rinshad"  };
 
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -70,6 +77,7 @@ public class UserTaskAssignmentActivity extends AppCompatActivity {
         submit = findViewById(R.id.submit);
         cancel = findViewById(R.id.cancel);
         createTask = findViewById(R.id.createTask);
+        spinnerAssignedTo = findViewById(R.id.spinner);
 
         createTask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,12 +91,111 @@ public class UserTaskAssignmentActivity extends AppCompatActivity {
               submitTask();
           }
       });
-      cancel.setOnClickListener(new View.OnClickListener() {
+        cancel.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
               finish();
           }
       });
+
+        spinnerElements();
+    }
+
+    private void spinnerElements() {
+
+        //TODO add or change methods accordingly
+        QueryRequestEnvelope envelope = getSpinnerData();
+
+        Call<ResponseQueryEnvelope> call = ApiClient.getApiClient().create(ApiInterface.class).fetchQueryData(envelope);
+        call.enqueue(new Callback<ResponseQueryEnvelope>() {
+            @Override
+            public void onResponse(Call<ResponseQueryEnvelope> call, Response<ResponseQueryEnvelope> response) {
+                Log.e("Success","Success");
+                spinnerAssignedTo.setAdapter(new ArrayAdapter<String>(UserTaskAssignmentActivity.this, android.R.layout.simple_spinner_dropdown_item, country));
+             /*  List<OutputFields> list =  response.body().getBody().getQueryDataResponse().getData().getDataSet().getDataRowList().get(0).getFieldData();
+               if (list != null && !list.isEmpty()) {
+                    for ( listItem : response.body().getBody().getQueryDataResponse().getData().getDataSet().getDataRowList()) {
+                      if (listItem.getColumn().equalsIgnoreCase("IsApproved"))
+                            mIsApproved = listItem.getVal();
+                        if (listItem.getColumn().equalsIgnoreCase("SC_Rejected"))
+                            mIsRejected = listItem.getVal();
+                        if (listItem.getColumn().equalsIgnoreCase("SC_Request_ID"))
+                            mRequestId = listItem.getVal();*//*
+
+                    }
+               }*/
+
+
+                Toast.makeText(UserTaskAssignmentActivity.this,"Success",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseQueryEnvelope> call, Throwable t) {
+                Log.e("Success","Success");
+                Toast.makeText(UserTaskAssignmentActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @NonNull
+    private QueryRequestEnvelope getSpinnerData() {
+        QueryRequestEnvelope envelope = new QueryRequestEnvelope();
+
+        QueryDataRequestBody body = new QueryDataRequestBody();
+
+        final QueryRequestData data = new QueryRequestData();
+
+        ModelCRUDRequest modelCRUDRequest = new ModelCRUDRequest();
+
+        ADLoginRequest loginRequest = new ADLoginRequest();
+
+        ModelCRUD modelCRUD = new ModelCRUD();
+
+        DataRowRequest dataRow = new DataRowRequest();
+
+
+        //TODO set all available data's here
+        loginRequest.setClientID(1000000);
+        loginRequest.setUser("SuperUser");
+        loginRequest.setPass("System");
+        loginRequest.setLang("en_US");
+        loginRequest.setRoleID(1000000);
+        loginRequest.setOrgID(1000000);
+        loginRequest.setWarehouseID(1000004);
+        loginRequest.setStage(0);
+
+
+        // Params inside dataRow as list (can use a loop or simply add objects to list
+        /*List<FieldData> fieldDataList = new ArrayList<>();
+
+        FieldData fieldData = new FieldData("SC_Request_ID","1000235");
+
+        fieldDataList.add(fieldData);
+
+        dataRow.setField(fieldDataList);*/
+
+        // Set modelCurd
+        modelCRUD.setDataRow(dataRow);
+        modelCRUD.setServiceType("MLW_UserQueryData");
+        modelCRUD.setTableName("AD_User");
+        //  modelCRUD.setAction("action");
+
+        // Set modelCurdRequest
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        modelCRUDRequest.setModelCRUD(modelCRUD);
+
+        // Set modelCurdRequest to QueryRequestData
+        data.setCduRequest(modelCRUDRequest);
+
+        //Set QueryRequestData object to QueryDataRequestBody
+        body.setUsStatesRequestData(data);
+
+        //Set QueryDataRequestBody object to RequestEnvelope
+        envelope.setBody(body);
+
+        modelCRUDRequest.setLoginRequest(loginRequest);
+        return envelope;
     }
 
     private void assignedUserTask() {
@@ -113,10 +220,9 @@ public class UserTaskAssignmentActivity extends AppCompatActivity {
                     if (list != null && !list.isEmpty()) {
                         for (OutputField listItem : list) {
                             if (listItem.getColumn().equalsIgnoreCase("AssignedTo")) {
+                                spinnerAssignedTo.setVisibility(View.GONE);
+                                assignedTo.setVisibility(View.VISIBLE);
                                 assignedTo.setText(listItem.getVal());
-                                assignedTo.setFocusable(false);
-                                assignedTo.setFocusableInTouchMode(false);
-                                assignedTo.setClickable(false);
                             }
                             if (listItem.getColumn().equalsIgnoreCase("Date1")) {
                                 assignedDate.setText(listItem.getVal());
